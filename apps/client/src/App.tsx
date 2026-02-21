084dab3a5c42992cdaedb80e223f26c8b128ecf7
Luseefor <sapanakosansar18@gmail.com>
2026-02-21 10:23:45 -0500

    fix: enable react-router v7 future flags to remove warnings

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -24,7 +24,7 @@ function LandingPage() {
 export default function App() {
     return (
         <TooltipProvider delayDuration={400}>
-            <BrowserRouter>
+            <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
                 <Routes>
                     <Route path="/" element={<LandingPage />} />
                     <Route path="/docs" element={<DocsPage />} />
